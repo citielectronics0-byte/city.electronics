c3b4dcb787ed54967ae8909c4b8a86b34bcd8142
citielectronics0-byte <citielectronics0@gmail.com>
2026-08-10 16:06:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,12 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  tanstackStart: {
+    spa: {
+      enabled: true,
+    },
+  },
+
   vite: {
     base: "/city.electronics/",
   },
