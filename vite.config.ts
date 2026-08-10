16aa0f49d6be6c98cc2a3e67022a234fbd9b407b
citielectronics0-byte <citielectronics0@gmail.com>
2026-08-10 15:40:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,18 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({});
+export default defineConfig({
+  tanstackStart: {
+    prerender: {
+      enabled: true,
+      autoSubfolderIndex: true,
+      autoStaticPathsDiscovery: true,
+      crawlLinks: true,
+      failOnError: true,
+    },
+  },
+
+  vite: {
+    base: "/city.electronics/",
+  },
+});
